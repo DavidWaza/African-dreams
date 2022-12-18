66ec8f7d51f2fe5c3c92be8309f68902a14642d3
David Waza <wazabanga00@gmail.com>
2022-12-18 01:54:30 +0100

    added framer motion to explore module

--- component/Explore/index.tsx
+++ component/Explore/index.tsx
@@ -3,6 +3,7 @@ import { Container, Col, Row } from "react-bootstrap";
 import styles from "../../styles/Home.module.css";
 import Image from "next/image";
 import Button from "../Button/button";
+import { motion } from "framer-motion";
 
 const Explore = () => {
   return (
@@ -10,7 +11,17 @@ const Explore = () => {
       <Container>
         <Row>
           <Col sm={6}>
-            <div className={styles.exploreWrapper}>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              // animate={{x: 100}}
+              transition={{
+                delay: 0.5,
+                duration: 0.8,
+                ease: [0, 0.71, 0.2, 1.01],
+              }}
+              className={styles.exploreWrapper}
+            >
               <Image
                 src="/Rectangle 8.png"
                 alt="image"
@@ -32,20 +43,38 @@ const Explore = () => {
                 height={150}
                 className={styles.image}
               />
-            </div>
+            </motion.div>
           </Col>
           <Col sm={6}>
-            <div className={styles.exploreTitle}>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{
+                delay: 0.8,
+                duration: 1,
+                ease: [0, 0.71, 0.2, 1.0],
+              }}
+              className={styles.exploreTitle}
+            >
               <p>Explore Nigeria with us </p>
               <p>
                 We are keen on bringing to life the beauty of our country
                 Nigeria. so we perform this task through adventures/expeditions
                 around the country.
               </p>
-            </div>
-            <div className={styles.exploreButton}>
+            </motion.div>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{
+                delay: 0.9,
+                duration: 1.1,
+                ease: [0, 0.71, 0.2, 1.0],
+              }}
+              className={styles.exploreButton}
+            >
               <Button title="Join Us" />
-            </div>
+            </motion.div>
           </Col>
         </Row>
       </Container>
