3c7ddc63d44c12caa231f901c34a874bd9502849
David Waza <wazabanga00@gmail.com>
2022-12-18 02:19:00 +0100

    added framer motion to location and suscribe module

--- component/Location/index.tsx
+++ component/Location/index.tsx
@@ -5,6 +5,7 @@ import styles from "../../styles/Home.module.css";
 import { Locations } from "../IconArr";
 import Button from "../Button/button";
 import Scroll from "../ReactScroll/Scroll";
+import { motion } from "framer-motion";
 
 type LocationType = {
   className: any;
@@ -18,12 +19,32 @@ const Location: React.FC<LocationType> = () => {
     <>
       <Container>
         <Row>
-          <div className={styles.locationTitle}>
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{
+              delay: 0.8,
+              duration: 1,
+              ease: [0, 0.71, 0.2, 1.0],
+            }}
+            className={styles.locationTitle}
+          >
             <p>Places we have Explored </p>
-          </div>
+          </motion.div>
           {Locations.map(({ id, image, title }) => (
             <Col sm={4} key={id}>
-              <div className={styles.location}>
+              <motion.div
+                initial={{ opacity: 0 }}
+                whileInView={{ opacity: 1 }}
+                transition={{
+                  default:{
+                    delay: 0.9,
+                    duration: 1.1,
+                    ease: [0, 0.99, 0.89, 1.01],
+                  }
+                }}
+                className={styles.location}
+              >
                 <Image
                   src="/star.png"
                   alt="star"
@@ -39,12 +60,21 @@ const Location: React.FC<LocationType> = () => {
                   className={styles.image}
                 />
                 <p className={styles.locationName}>{title}</p>
-              </div>
+              </motion.div>
             </Col>
           ))}
-          <div className={styles.locationButton}>
+          <motion.div
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            transition={{
+              delay: 0.9,
+              duration: 1.1,
+              ease: [0, 0.71, 0.2, 1.0],
+            }}
+            className={styles.locationButton}
+          >
             <Button title="Join Us" />
-          </div>
+          </motion.div>
         </Row>
         <div className={styles.LocationMobileView}>
           <Scroll />
--- component/Subscribe/index.tsx
+++ component/Subscribe/index.tsx
@@ -4,6 +4,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import styles from "../../styles/Home.module.css";
 import Image from "next/image";
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 const Subscribe = () => {
   return (
@@ -11,13 +12,25 @@ const Subscribe = () => {
       <Container>
         <Row>
           <Col>
-            <div className={styles.subscribe}>
+            <motion.div
+              initial={{ opacity: 0 }}
+              whileInView={{ opacity: 1 }}
+              transition={{
+                delay: 0.8,
+                duration: 1,
+                ease: [0, 0.71, 0.2, 1.0],
+              }}
+              className={styles.subscribe}
+            >
               <div className={styles.subScribeContainer}>
                 <Row>
                   <Col sm={6}>
                     <div className={styles.campContent}>
                       <p>Let's stay in Touch</p>
-                      <p>Keep updated with our travel <br />plans and adventures.</p>
+                      <p>
+                        Keep updated with our travel <br />
+                        plans and adventures.
+                      </p>
                       <form className={styles.formContainer}>
                         <input type="text" />
                         <div className={styles.iconSendContainer}>
@@ -49,7 +62,7 @@ const Subscribe = () => {
                   </Col>
                 </Row>
               </div>
-            </div>
+            </motion.div>
           </Col>
         </Row>
       </Container>
