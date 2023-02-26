e9fe4d17364ce76f1c7889790008a37e7d64daec
David Waza <wazabanga00@gmail.com>
2023-02-26 01:13:35 +0100

    created the custom 404 page

---
+++ pages/404.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
+import Image from "next/image";
+
+const NotFound = () => {
+  return (
+    <div className={styles.notFoundBG}>
+      <div className={styles.notFoundIMG}>
+        <Image src="/wolf.png" alt="wolf" height={170} width={200} />
+      </div>
+      <div className={styles.notFound}>
+        <p>
+          ooppps!! You are wide into the &nbsp;<span>jungle</span>
+        </p>
+        <p>
+          Kindly go back to &nbsp;<Link href="/">Camp!</Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+export default NotFound;
